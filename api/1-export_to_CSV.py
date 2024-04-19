923af34930f2b8fdf7c70904ebf4d2eeaf6b6a3e
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-19 17:45:24 -0400

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -14,27 +14,30 @@ def csv_format():
     
     user_response = requests.get(api_url)
     user_data = user_response.json()
-    
+
     if isinstance(user_data, list):
         user_data = user_data[0]
-    
+
     EMPLOYEE_NAME = user_data.get('name')
-    
+
     todos_response = requests.get(api_url2)
     todos_data = todos_response.json()
-    
+
     f_name = f"{u_id}.csv"
-    
+
     with open(f_name, 'w', newline='', encoding='utf-8') as f:
         csvwriter = csv.writer(f)
-        
-        csvwriter.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        
+
+        csvwriter.writerow(["USER_ID", "USERNAME",
+                            "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
         for info in todos_data:
             TASK_COMPLETED_STATUS = info.get("completed")
             TASK_TITLE = info.get("title")
-            
-            csvwriter.writerow([u_id, EMPLOYEE_NAME, TASK_COMPLETED_STATUS, TASK_TITLE])
+
+            csvwriter.writerow([u_id, EMPLOYEE_NAME,
+                                TASK_COMPLETED_STATUS, TASK_TITLE])
+
 
 if __name__ == '__main__':
     if len(argv) != 2:
