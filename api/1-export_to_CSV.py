8f11b0b6ca12e7f820d12b608313340a4ee32394
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-19 20:39:15 -0400

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
 '''Export data in the CSV format'''
 
-import csv
 import requests
 import sys
 
 
 def csv_format():
 
-    u_id = sys.argv[1]
-
-    user_url = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' + u_id).json()
-    NAME = user_url.get('username')
-    todos = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' + u_id + '/todos')
-    f_name = u_id + '.csv'
-    with open(f_name, 'w', encoding='utf-8') as f:
-        for info in todos:
+    id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}"\
+        .format(id)
+    response = requests.get(user_url).json()
+    NAME = response.get('username')
+    response = requests.get(url).json()
+    with open("{}.csv".format(id), 'w') as f:
+        for info in response:
             TASK_COMPLETED_STATUS = info.get("completed")
             TASK_TITLE = info.get("title")
             f.write('"{}","{}","{}","{}"\n'.format(
-                u_id, NAME, TASK_COMPLETED_STATUS, TASK_TITLE))
+                id, NAME, TASK_COMPLETED_STATUS, TASK_TITLE))
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     csv_format()
