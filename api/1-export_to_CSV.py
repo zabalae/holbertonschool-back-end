2589abcaa7269363cb776b5aa9a953f32b8b27cb
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-19 22:16:58 -0400

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,48 +2,19 @@
 """RESTful API for employee"""
 import csv
 import requests
-import sys
-
-
-def get_employee_tasks(employee_id):
-
-    employee_name = ""
-    task_data = []
-
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    url_T = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-
-    if isinstance(user_data, list):
-        user_data = user_data[0]
-
-    employee_name = user_data.get('name')
-
-    tasks_response = requests.get(url_T)
-    tasks_data = tasks_response.json()
-
-    for task in tasks_data:
-        task_data.append([
-            employee_id,
-            employee_name,
-            str(task['completed']),
-            task['title']
-        ])
-
-    filename = f"{employee_id}.csv"
-    with open(filename, 'w') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        for i in task_data:
-            writer.writerow([employee_id, employee_name,
-                             i.get('completed'), i.get('title')])
+from sys import argv
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"UsageError: python3 {__file__} employee_id(int)")
-        sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    get_employee_tasks(employee_id)
+    user_id = argv[1]
+
+    user_url = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + user_id).json()
+    NAME = user_url.get('username')
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + user_id + '/todos')
+    with open("{}.csv".format(user_id), 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for x in todos.json():
+            writer.writerow([user_id, NAME, x.get('completed'), x.get('title')])
