b90e52c8ca18c1cdc76cc7453bd3acff19e717fa
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-19 21:14:47 -0400

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -36,7 +36,8 @@ def get_employee_tasks(employee_id):
     filename = f"{employee_id}.csv"
     with open(filename, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        csvwriter.writerow(["USER_ID","USERNAME", "TASK_COMPLETED_STATUS",
+                            "TASK_TITLE"])
         csvwriter.writerows(todos_data)
 
 
