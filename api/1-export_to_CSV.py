0f35e5de4585f6b3662626d1737528c4e39ea5c1
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-19 21:13:34 -0400

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,25 +1,49 @@
 #!/usr/bin/python3
-'''Export data in the CSV format'''
+"""RESTful API for employee"""
+
+import csv
 import requests
 import sys
 
 
-def csv_format():
+def get_employee_tasks(employee_id):
 
-    id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}"\
-        .format(id)
-    response = requests.get(user_url).json()
-    NAME = response.get('username')
-    response = requests.get(url).json()
-    with open("{}.csv".format(id), 'w') as f:
-        for info in response:
-            TASK_COMPLETED_STATUS = info.get("completed")
-            TASK_TITLE = info.get("title")
-            f.write('"{}","{}","{}","{}"\n'.format(
-                id, NAME, TASK_COMPLETED_STATUS, TASK_TITLE))
+    employee_name = ""
+    number_of_done_tasks = 0
+    total_number_of_tasks = 0
+    task_titles = []
+
+    url_users = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    url_T = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+
+    user_response = requests.get(url_users)
+    user_data = user_response.json()
+
+    if isinstance(user_data, list):
+        user_data = user_data[0]
+
+    employee_name = user_data.get('name')
+
+    todos_response = requests.get(url_T)
+    todos_data = todos_response.json()
+
+    for task in todos_data:
+        total_number_of_tasks += 1
+        if task['completed']:
+            number_of_done_tasks += 1
+            task_titles.append(task['title'])
+
+    filename = f"{employee_id}.csv"
+    with open(filename, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        csvwriter.writerows(todos_data)
 
 
-if __name__ == '__main__':
-    csv_format()
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"UsageError: python3 {__file__} employee_id(int)")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_tasks(employee_id)
